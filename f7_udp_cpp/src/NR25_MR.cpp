d9da70f6651095357e9da4dedee33aec8031e319
K.Tashiro <tashikou1682@gmail.com>
2025-02-03 16:04:35 +0900

    MR機構制御実装

--- f7_udp_cpp/src/NR25_MR.cpp
+++ f7_udp_cpp/src/NR25_MR.cpp
@@ -18,12 +18,10 @@ int roller_speed_shoot_cd = 50;
 int roller_speed_reload = 10;
 
 // IPアドレスとポートの指定
-std::string udp_ip =
-    "192.168.8.215"; // 送信先IPアドレス、宛先マイコンで設定したIPv4アドレスを指定
-int udp_port =
-    5000; // 送信元ポート番号、宛先マイコンで設定したポート番号を指定
+std::string udp_ip = "192.168.8.215"; // 送信先IPアドレス、宛先マイコンで設定したIPv4アドレスを指定
+int udp_port = 5000;                  // 送信元ポート番号、宛先マイコンで設定したポート番号を指定
 
-std::vector<int> data = {0, 0, 0, 0, 0, 0, -1, -1, -1}; // グローバル変数
+std::vector<int> data = {0, 0, 0, 0, 0, 0, -1, -1, -1}; // 7~9番を電磁弁制御に転用中（-1 or 1）
 
 // 各機構のシーケンスを格納するクラス
 class Action {
@@ -146,14 +144,24 @@ private:
 
         // bool SHARE = msg->buttons[8];
         // bool OPTION = msg->buttons[9];
-        // bool PS = msg->buttons[10];
+        bool PS = msg->buttons[10];
 
         // bool L3 = msg->buttons[11];
         // bool R3 = msg->buttons[12];
+        /*
+                if (PS) {
+                    while (1) {
+                        std::fill(data.begin(), data.end(), 0);
+                        udp_.send(data);
+                        std::cout << "！緊急停止中！" << std::endl;
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                    }
+                }
+        */
 
         if (CIRCLE) {
             Action::ready_for_shoot_action(udp_);
-            std::this_thread::sleep_for(std::chrono::seconds(3));
+            std::this_thread::sleep_for(std::chrono::milliseconds(3000));
         }
 
         if (Action::ready_for_shoot) {
