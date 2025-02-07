7b74e9540dcce2aa8c3bdbd6a80a216c0c7e54b3
mo-ri-tania <i.morimoto.05150@gmail.com>
2025-02-08 03:00:10 +0900

    ステア角＋YAW回転

--- f7_udp_cpp/src/NR25_MR_SwerveDrive.cpp
+++ f7_udp_cpp/src/NR25_MR_SwerveDrive.cpp
@@ -13,6 +13,7 @@ RRST NHK2025
 #include <thread>
 
 int deg;
+int truedeg;
 
 // IPアドレスとポートの指定
 std::string udp_ip = "192.168.8.215"; // 送信先IPアドレス、宛先マイコンで設定したIPv4アドレスを指定
@@ -22,6 +23,7 @@ std::vector<int> data = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int wheelspeed = 10;
 float deadzone = 0.3;
+int yawspeed = 10;
 
 class PS4_Listener : public rclcpp::Node {
 public:
@@ -41,8 +43,8 @@ private:
     void ps4_listener_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
         float LS_X = -1 * msg->axes[0];
         float LS_Y = msg->axes[1];
-        //  float RS_X = -1 * msg->axes[3];
-        //  float RS_Y = msg->axes[4];
+        float RS_X = -1 * msg->axes[3];
+        //float RS_Y = msg->axes[4];
 
         // bool CROSS = msg->buttons[0];
         // bool CIRCLE = msg->buttons[1];
@@ -57,7 +59,7 @@ private:
         // bool L1 = msg->buttons[4];
         // bool R1 = msg->buttons[5];
 
-        // float L2 = (-1 * msg->axes[2] + 1) / 2;
+        //float L2 = (-1 * msg->axes[2] + 1) / 2;
         float R2 = (-1 * msg->axes[5] + 1) / 2;
 
         // bool SHARE = msg->buttons[8];
@@ -80,14 +82,22 @@ private:
 
         float rad = atan2(LS_Y, LS_X);
         deg = rad * 180 / M_PI;
-
-        if ((fabs(LS_X) <= deadzone) && (fabs(LS_Y) <= deadzone)) {
+        //XY座標での正しい角度truedeg
+        truedeg = deg;
+        if((0<=truedeg)&&(truedeg<=180)){
+            truedeg = truedeg;
+        }
+        if((-180<=truedeg)&&(truedeg<=0)){
+            truedeg = -truedeg + 360;
+        }
+        //deadzone追加
+        if ((fabs(LS_X) <= deadzone) && (fabs(LS_Y) <= deadzone)&& (fabs(RS_X) <= deadzone)) {
             data[1] = 0;
             data[2] = 0;
             data[3] = 0;
             data[4] = 0;
         }
-
+        //XY座標での９０度の位置に１３５度を変換して計算
         if ((-180 <= deg) && (deg <= -135)) {
             deg = -deg - 135;
         } else {
@@ -102,13 +112,36 @@ private:
 
         // 進行方向にするため初期位置を135→145
         if ((fabs(LS_X) <= deadzone) && (fabs(LS_Y) <= deadzone)) {
-            deg = 145;
+            deg = 135;
             data[5] = deg;
             data[6] = deg;
             data[7] = deg;
             data[8] = deg;
         }
-
+        // //時計回りYAW回転
+        // if (RS_X < 0){
+        //     data[5] = 180;
+        //     data[6] = 90;
+        //     data[7] = 90;
+        //     data[8] = 180;
+        //     data[1] = -yawspeed;
+        //     data[2] = yawspeed;
+        //     data[3] = -yawspeed;
+        //     data[4] = yawspeed;
+        // }
+        // //半時計回りYAW回転
+        // if (0 <= RS_X){
+        //     data[5] = 180;
+        //     data[6] = 90;
+        //     data[7] = 90;
+        //     data[8] = 180;
+        //     data[1] = yawspeed;
+        //     data[2] = -yawspeed;
+        //     data[3] = yawspeed;
+        //     data[4] = -yawspeed;
+        // }
+        
+        //独ステが扱えない範囲の変換
         if ((270 < deg) && (deg < 360)) {
             deg = deg - 180;
             data[5] = deg;
@@ -144,20 +177,22 @@ private:
             data[4] = wheelspeed * R2;
         }
         if (UP) {
-            data[5] = 145;
-            data[6] = 145;
-            data[7] = 145;
-            data[8] = 145;
+            deg = 135;
+            data[5] = deg;
+            data[6] = deg;
+            data[7] = deg;
+            data[8] = deg;
             data[1] = -wheelspeed * R2;
             data[2] = -wheelspeed * R2;
             data[3] = -wheelspeed * R2;
             data[4] = -wheelspeed * R2;
         }
         if (DOWN) {
-            data[5] = 145;
-            data[6] = 145;
-            data[7] = 145;
-            data[8] = 145;
+            deg = 135;
+            data[5] = deg;
+            data[6] = deg;
+            data[7] = deg;
+            data[8] = deg;
             data[1] = wheelspeed * R2;
             data[2] = wheelspeed * R2;
             data[3] = wheelspeed * R2;
@@ -170,14 +205,14 @@ private:
         data[8] = deg;
 
         // わかりやすいように数値を45-135-225座標に合わせてます。不要なら消去で
-        if ((deg > 45) && (deg <= 135)) {
-            int adj_deg = int(deg * 0.9);
-            std::cout << adj_deg << std::endl;
-        }
-        if (((deg <= 45) && (deg > 135))) {
-            std::cout << deg - 10 << std::endl;
-        }
-        // std::cout << deg << std::endl;
+        // if ((deg > 45) && (deg <= 135)) {
+        //     int adj_deg = int(deg * 0.9);
+        //     std::cout << adj_deg << std::endl;
+        // }
+        // if (((deg <= 45) && (deg > 135))) {
+        //     std::cout << deg - 10 << std::endl;
+        // }
+        std::cout << data[1] << std::endl;
 
         // std::cout << data << std::endl;
         udp_.send(data);
