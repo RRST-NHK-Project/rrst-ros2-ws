54c26e8dbcc9fa8a4e32ae792cfa8b8704866f32
K.Tashiro <tashikou1682@gmail.com>
2025-04-07 13:41:50 +0900

    f7_udpを削除しf7_udp_cpp, ros2udpを統合。マイコン側PIOプロジェクト群を同梱するように変更。

--- ros2udp/launch/nr25_dr.py
+++ ros2udp/launch/nr25_dr.py
@@ -1,27 +1,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='joy',
-            executable='joy_node',
-            name='joy',
-            output='screen',
-            emulate_tty=True
-        ),
-        Node(
-            package='ros2udp',
-            executable='nr25_dr_omni',
-            name='dr_omni',
-            output='screen',
-            emulate_tty=True
-        ),
-        Node(
-            package='ros2udp',
-            executable='nr25_dr',
-            name='dr',
-            output='screen',
-            emulate_tty=True
-        ),
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="joy",
+                executable="joy_node",
+                output="screen",
+                parameters=[{"device_id": 0}],  # ここで device_id を指定
+                remappings=[("/joy", "/joy1")],
+            ),
+            Node(
+                package="ros2udp",
+                executable="nr25_dr_sd",
+                output="screen",
+            ),
+            Node(
+                package="ros2udp",
+                executable="nr25_dr_c610",
+                output="screen",
+            ),
+        ]
+    )
