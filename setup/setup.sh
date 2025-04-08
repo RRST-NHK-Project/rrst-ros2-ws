5a32826a34eb9275cc2cbe496b603b8f9cf82f8a
K.Tashiro <tashikou1682@gmail.com>
2025-04-09 02:14:49 +0900

    セットアップスクリプトの更新

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,18 +8,18 @@
 
 
 # いつもの
-sudo apt update -y
+sudo apt-get update -y
 
 # figletのインストール
-sudo apt install -y figlet
+sudo apt-get install -y figlet
 
 # 以下LD19用
 cd ~/ros2_ws/src/ldrobot-lidar-ros2/scripts/
 ./create_udev_rules.sh
-sudo apt install ros-jazzy-nav2-lifecycle-manager
-sudo apt install libudev-dev
-sudo apt install -y ros-${ROS_DISTRO}-diagnostic-updater
-sudo apt install -y ros-${ROS_DISTRO}-nav2-util
+sudo apt-get install ros-jazzy-nav2-lifecycle-manager
+sudo apt-get install libudev-dev
+sudo apt-get install -y ros-${ROS_DISTRO}-diagnostic-updater
+sudo apt-get install -y ros-${ROS_DISTRO}-nav2-util
 
 # 他のスクリプトに一括で実行権限を渡す
 sudo chmod +x ~/ros2_ws/src/setup/*.sh
