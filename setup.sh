79b1f6fd388fedf3084e45d5a4476f0dc3fd7923
K.Tashiro <tashikou1682@gmail.com>
2025-04-08 23:14:57 +0900

    セットアップスクリプトの更新

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
+"""
+依存関係の一括インストール
+
+＊実行方法＊
+cd ~/ros2_ws/src
+sudo chmod +x setup.sh
+./setup.sh
+
+"""
+
+# いつもの
 sudo apt update -y
 sudo apt upgrade -y
+
+# figletのインストール
 sudo apt install -y figlet
+
+# 以下LD19用
 cd ~/ros2_ws/src/ldrobot-lidar-ros2/scripts/
 ./create_udev_rules.sh
 sudo apt install libudev-dev
