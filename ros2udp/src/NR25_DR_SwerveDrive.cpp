487aed5e13c3887fdca67fe8a1f4939ad4c27397
K.Tashiro <tashikou1682@gmail.com>
2025-04-19 02:53:56 +0900

    自動加速（20%）実機テストクリア

--- ros2udp/src/NR25_DR_SwerveDrive.cpp
+++ ros2udp/src/NR25_DR_SwerveDrive.cpp
@@ -108,7 +108,7 @@ public:
     // 高速自動走行（自動加速、障害物を検知したら停止）
     static void automatic_cruise(UDP &udp) {
         const int steps = 20;          // 加減速のステップ数
-        const double maxOutput = 90.0; // 最大出力
+        const double maxOutput = 20.0; // 最大出力
         // const int cruiseTimeMs = 2000; // 巡航時間（ミリ秒）
         const int intervalMs = 50; // ステップごとの待機時間
         bool skip_while = false;
@@ -130,10 +130,10 @@ public:
             }
             double t = static_cast<double>(i) / steps; // 0〜1
             double output = maxOutput * t * t;         // 2次関数加速
-            data[1] = output;
-            data[2] = output;
-            data[3] = output;
-            data[4] = output;
+            data[1] = -output;
+            data[2] = -output;
+            data[3] = -output;
+            data[4] = -output;
             std::cout << output << std::endl;
             udp.send(data);
             std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
@@ -153,10 +153,10 @@ public:
                     std::cout << "<停止！>" << std::endl;
                     break;
                 }
-                data[1] = maxOutput;
-                data[2] = maxOutput;
-                data[3] = maxOutput;
-                data[4] = maxOutput;
+                data[1] = -maxOutput;
+                data[2] = -maxOutput;
+                data[3] = -maxOutput;
+                data[4] = -maxOutput;
                 udp.send(data);
                 std::cout << "<巡航中>" << std::endl;
                 std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
@@ -646,7 +646,7 @@ private:
         min_distance = msg->data[0];
         // std::cout << min_distance << std::endl;
         // 障害物の有無（しきい値は要調整）
-        if (min_distance < 0.5) {
+        if (min_distance < 1.1) {
             front_cleared = false;
         } else {
             front_cleared = true;
