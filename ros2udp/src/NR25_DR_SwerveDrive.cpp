bdc62f95d7652d79d63be15049d256672fa1a257
mo-ri-tania <i.morimoto.05150@gmail.com>
2025-05-16 13:10:33 +0900

    モーターかくつき無くした

--- ros2udp/src/NR25_DR_SwerveDrive.cpp
+++ ros2udp/src/NR25_DR_SwerveDrive.cpp
@@ -567,6 +567,8 @@ private:
         // std::cout << data[8] << ", " << data[9] << ", " << data[10] << ", " << data[11] << ", ";
         // std::cout << data[12] << ", " << data[13] << ", " << data[14] << ", " << data[15] << ", ";
         // std::cout << data[16] << ", " << data[17] << ", " << data[18] << std::endl;
+        //std::cout << data[1] << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
         // 現在の状態を次回のために保存
         last_option = OPTION;
