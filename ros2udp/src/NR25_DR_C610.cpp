516b0e88cc6b9d65d6de22dc288b4cbd3c9b512d
takashinosuke <tiramisu-chaohan.8989@docomo.ne.jp>
2025-05-29 17:51:44 +0900

    二段階修正

--- ros2udp/src/NR25_DR_C610.cpp
+++ ros2udp/src/NR25_DR_C610.cpp
@@ -26,7 +26,7 @@ int motor4 = 50;
 int hcsr04 = 0;
 bool UP_state = false;
 bool Nomal_mode = false;
-
+//rbool data[14]=false;
 // IPアドレスとポートの指定
 std::string udp_ip = "192.168.0.218"; // 送信先IPアドレス、宛先マイコンで設定したIPv4アドレスを指定
 int udp_port = 5000;                  // 送信元ポート番号、宛先マイコンで設定したポート番号を指定
@@ -66,7 +66,7 @@ public:
 
     static void ready_for_dunk_action(UDP &udp) {
         std::cout << "ダンク待機開始" << std::endl;
-        std::cout << "１段階展開[1]" << std::endl;
+        std::cout << "１段階展開[11]" << std::endl;
         data[11] = 1;
         udp.send(data);
         ready_for_dunk = true;
@@ -76,13 +76,13 @@ public:
     static void dunk_shoot_action(UDP &udp) {
         std::cout << "<ダンクシーケンス開始>" << std::endl;
 
-        std::cout << "２段階展開[15]＋トリガー[13]" << std::endl;
-        data[15] = 1;
+        std::cout << "２段階展開[12]＋トリガー[13]" << std::endl;
+        data[12] = 1;
 
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        std::cout << "２段階展開[15]＋トリガー[13]" << std::endl;
+        std::cout << "２段階展開[12]＋トリガー[13]" << std::endl;
 
         data[13] = 1;
         udp.send(data);
@@ -92,18 +92,16 @@ public:
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(730));
         // シュートはsleep 660
-        std::cout << "格納[18]+チルト展開[16]" << std::endl;
-        data[18] = 1;
-        data[16] = 1;
+        std::cout << "格納[15]" << std::endl;
+        data[15] = 1;
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 要調整
-        std::cout << "１段階格納[11]＋２段階格納[15]＋チルト格納[16]" << std::endl;
+        std::cout << "１段階格納[11]＋２段階格納[12]" << std::endl;
         data[11] = 0;
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(500)); // 要調整
 
-        data[15] = 0;
-        data[16] = 0;
+        data[12] = 0;
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 要調整
 
@@ -116,13 +114,13 @@ public:
         std::cout << "<ダンクシーケンス終了>" << std::endl;
     }
 
-    static void pass_shoot_action(UDP &udp) { // パス、シュート
+   /* static void pass_shoot_action(UDP &udp) { // パス、シュート
         std::cout << "パス、シュート開始" << std::endl;
-        std::cout << "１段階展開[11]＋２段階展開[15]" << std::endl;
+        std::cout << "１段階展開[11]＋２段階展開[12]" << std::endl;
         data[11] = 1;
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        data[15] = 1;
+        data[12] = 1;
         udp.send(data);
         std::this_thread::sleep_for(std::chrono::milliseconds(400)); // 要調整
         std::cout << "ハンド展開" << std::endl;
@@ -136,8 +134,8 @@ public:
         udp.send(data);
         std::cout << "パス、シュート完了" << std::endl;
     }
-
-    static void dribble_action(UDP &udp) {
+*/
+  /*  static void dribble_action(UDP &udp) {
         std::cout << "ドリブル開始" << std::endl;
         std::cout << "１段階展開[11]＋２段階展開[15]" << std::endl;
         data[11] = 1;
@@ -153,7 +151,7 @@ public:
         data[15] = 0;
         udp.send(data);
         std::cout << "ドリブル完了" << std::endl;
-    }
+    }*/
 };
 bool Action::ready_for_dunk = false;
 
@@ -268,15 +266,15 @@ private:
         // }
 
         // １段階展開[11]＋２段階展開[15] を同時に行うボタン
-        if (L2 < 0.5 && Nomal_mode==0) {
-            data[11] = 0;
-            data[15] = 0;
+        if (L2 < 0.5 ) {
+           // data[11] = 0;
+            data[12] = 0;
             // std::cout << "格納"<< std::endl;
             //  std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
         if (L2 >= 0.5) {
             data[11] = 1;
-            data[15] = 1;
+            data[12] = 1;
             // std::cout << "展開"<< std::endl;
             //  std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
@@ -295,11 +293,11 @@ private:
         // std::cout << data[16] << std::endl;
 
         // デバッグ用 *NOTE:for文でcoutするとカクつくからこの記述
-         //std::cout << data[0] << ", " << data[1] << ", " << data[2] << ", " << data[3] << ", ";
-         //std::cout << data[4] << ", " << data[5] << ", " << data[6] << ", " << data[7] << ", ";
-        //std::cout << data[8] << ", " << data[9] << ", " << data[10] << ", " << data[11] << ", ";
-        //std::cout << data[12] << ", " << data[13] << ", " << data[14] << ", " << data[15] << ", ";
-        ///std::cout << data[16] << ", " << data[17] << ", " << data[18] << std::endl;
+         std::cout << data[0] << ", " << data[1] << ", " << data[2] << ", " << data[3] << ", ";
+         std::cout << data[4] << ", " << data[5] << ", " << data[6] << ", " << data[7] << ", ";
+        std::cout << data[8] << ", " << data[9] << ", " << data[10] << ", " << data[11] << ", ";
+        std::cout << data[12] << ", " << data[13] << ", " << data[14] << ", " << data[15] << ", ";
+        std::cout << data[16] << ", " << data[17] << ", " << data[18] << std::endl;
         //std::cout << data[11] << std::endl;
         // std::cout << msg->buttons[5] << std::endl;
         udp_.send(data);
