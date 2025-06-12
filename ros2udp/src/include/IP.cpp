484b221e1e558fd0901928ad2a5adfc74ad20c1a
KouTashi <tashikou1682@gmail.com>
2025-06-12 12:52:55 +0900

    テスト用IPアドレスの変更

--- ros2udp/src/include/IP.cpp
+++ ros2udp/src/include/IP.cpp
@@ -13,9 +13,11 @@ const std::string IP_MR_SD = "192.168.8.215";
 const std::string IP_MR = "192.168.8.216";
 const std::string IP_DR_SD = "192.168.0.217";
 const std::string IP_DR = "192.168.0.218";
+const std::string IP_TEST = "192.168.128.205";
 
 // 送信先ポート番号、宛先マイコンで設定したポート番号を指定
 const int PORT_MR_SD = 5000;
 const int PORT_MR = 5000;
 const int PORT_DR_SD = 5000;
 const int PORT_DR = 5000;
+const int PORT_TEST = 5000;
