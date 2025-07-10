b335be7b4ab09aaa756a5e694ef2b3872f181093
KouTashi <tashikou1682@gmail.com>
2025-07-10 22:16:48 +0900

    ワイヤレスデバッグ実装途中

--- microcontroller/esp32/ros2esp_arduino/ros2esp_arduino.ino
+++ microcontroller/esp32/ros2esp_arduino/ros2esp_arduino.ino
@@ -7,7 +7,8 @@
 #include "BluetoothSerial.h"
 
 BluetoothSerial SerialBT;
-#define MAX_ARRAY_SIZE 18
+
+#define MAX_ARRAY_SIZE 19
 
 rcl_subscription_t subscriber;
 std_msgs__msg__Int32MultiArray msg;
@@ -17,34 +18,50 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 int32_t buffer[MAX_ARRAY_SIZE];
 
+// ★ 受信データ格納用（グローバル）
+volatile int32_t received_data[MAX_ARRAY_SIZE];  // 最新の受信データ
+volatile size_t received_size = 0;               // 受信データのサイズ
+volatile bool new_data_received = false;         // 新しいデータの有無フラグ
+
 #define RCCHECK(fn) { if ((fn) != RCL_RET_OK) error_loop(); }
 
 void error_loop() {
-  while (1) delay(100);
-}
-
-void subscription_callback(const void *msgin) {
-  const std_msgs__msg__Int32MultiArray *msg = (const std_msgs__msg__Int32MultiArray *)msgin;
-  if (msg->data.size > 0) {
-    if (msg->data.data[0] == 1) {
-      for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
-        SerialBT.print(msg->data.data[i]);
-        SerialBT.print(", ");
-      }
-      SerialBT.println("");
-    }
-    digitalWrite(5, msg->data.data[0] == 1 ? HIGH : LOW);
+  while (1) {
+    SerialBT.println("RCL Error!");
+    delay(1000);
   }
 }
 
+// ★ コールバック内でグローバル変数にコピー
+void subscription_callback(const void *msgin) {
+  const std_msgs__msg__Int32MultiArray *msg = (const std_msgs__msg__Int32MultiArray *)msgin;
+
+  size_t len = msg->data.size;
+  if (len > MAX_ARRAY_SIZE) len = MAX_ARRAY_SIZE;
+
+  for (size_t i = 0; i < len; i++) {
+    received_data[i] = msg->data.data[i];
+  }
+
+  received_size = len;
+  new_data_received = true;
+}
+
 void setup() {
-  Serial.begin(115200);
   SerialBT.begin("ESP32");
   delay(2000);
+
   set_microros_transports();
+  SerialBT.println("Bluetooth Serial Started.");
 
   allocator = rcl_get_default_allocator();
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // ★ エージェントと接続できるまでリトライ
+  while (rclc_support_init(&support, 0, NULL, &allocator) != RCL_RET_OK) {
+    SerialBT.println("Waiting for agent...");
+    delay(1000);  // 1秒待つ
+  }
+
   RCCHECK(rclc_node_init_default(&node, "micro_ros_esp_node_00", "", &support));
   RCCHECK(rclc_subscription_init_default(
     &subscriber,
@@ -59,10 +76,27 @@ void setup() {
 
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
   pinMode(5, OUTPUT);
 }
 
 void loop() {
   RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
-  delay(10);
+
+  // ★ 新しいデータがあれば処理
+  if (new_data_received) {
+    new_data_received = false;
+
+    SerialBT.print("Received: ");
+    for (size_t i = 0; i < received_size; i++) {
+      SerialBT.print(received_data[i]);
+      SerialBT.print(", ");
+    }
+    SerialBT.println();
+
+    // 例：LEDを制御
+    digitalWrite(5, received_data[2] == 1 ? HIGH : LOW);
+  }
+
+  //delay(10);
 }
---
+++ ros2esp/src/NR25_MR_SwerveDrive.cpp
@@ -0,0 +1,132 @@
+/*
+RRST-NHK-Project 2025
+PS4コントローラーの入力を取得するサンプルプログラム
+*/
+
+// 標準
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
+// ROS
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
+
+#define MC_PRINTF 1 // マイコン側のprintfを無効化・有効化(0 or 1)
+
+std::vector<int32_t> data(19, 0); // マイコンに送信される配列"data"
+/*
+マイコンに送信される配列"data"
+debug: マイコンのprintfを有効化, MD: モータードライバー, TR: トランジスタ
+| data[n] | 詳細 | 範囲 |
+| ---- | ---- | ---- |
+| data[0] | debug | 0 or 1 |
+| data[1] | MD1 | -100 ~ 100 |
+| data[2] | MD2 | -100 ~ 100 |
+| data[3] | MD3 | -100 ~ 100 |
+| data[4] | MD4 | -100 ~ 100 |
+| data[5] | MD5 | -100 ~ 100 |
+| data[6] | MD6 | -100 ~ 100 |
+| data[7] | Servo1 | 0 ~ 270 |
+| data[8] | Servo2 | 0 ~ 270 |
+| data[9] | Servo3 | 0 ~ 270 |
+| data[10] | Servo4 | 0 ~ 270 |
+| data[11] | TR1 | 0 or 1|
+| data[12] | TR2 | 0 or 1|
+| data[13] | TR3 | 0 or 1|
+| data[14] | TR4 | 0 or 1|
+| data[15] | TR5 | 0 or 1|
+| data[16] | TR6 | 0 or 1|
+| data[17] | TR7 | 0 or 1|
+| data[18] | TR8 | 0 or 1|
+*/
+
+class PS4_Listener : public rclcpp::Node {
+public:
+    PS4_Listener()
+        : Node("ps4_listener") {
+
+        subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+            "joy", 10,
+            std::bind(&PS4_Listener::ps4_listener_callback, this,
+                      std::placeholders::_1));
+
+        publisher_ = this->create_publisher<std_msgs::msg::Int32MultiArray>("mr_swerve_drive", 10);
+
+        RCLCPP_INFO(this->get_logger(),
+                    "PS4 Listener initialized");
+    }
+
+private:
+    void ps4_listener_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
+
+        // コントローラーの入力を取得、使わない入力はコメントアウト推奨
+        //  float LS_X = -1 * msg->axes[0];
+        //  float LS_Y = msg->axes[1];
+        //  float RS_X = -1 * msg->axes[3];
+        //  float RS_Y = msg->axes[4];
+
+        // bool CROSS = msg->buttons[0];
+        bool CIRCLE = msg->buttons[1];
+        // bool TRIANGLE = msg->buttons[2];
+        // bool SQUARE = msg->buttons[3];
+
+        // bool LEFT = msg->axes[6] == 1.0;
+        // bool RIGHT = msg->axes[6] == -1.0;
+        // bool UP = msg->axes[7] == 1.0;
+        // bool DOWN = msg->axes[7] == -1.0;
+
+        // bool L1 = msg->buttons[4];
+        //  bool R1 = msg->buttons[5];
+
+        // float L2 = (-1 * msg->axes[2] + 1) / 2;
+        float R2 = (-1 * msg->axes[5] + 1) / 2;
+
+        // bool SHARE = msg->buttons[8];
+        // bool OPTION = msg->buttons[9];
+        // bool PS = msg->buttons[10];
+
+        // bool L3 = msg->buttons[11];
+        // bool R3 = msg->buttons[12];
+
+        data[0] = MC_PRINTF; // マイコン側のprintfを無効化・有効化(0 or 1)
+
+        if (CIRCLE) {
+            std::cout << "CIRCLE" << std::endl;
+            data[2] = 1;
+            publish_data();
+        } else {
+            data[2] = 0; // CIRCLEボタンが押されていない場合は0に設定
+        }
+
+        data[3] = R2 * 100;
+        publish_data();
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+
+    void publish_data() {
+        auto msg = std_msgs::msg::Int32MultiArray();
+        msg.data.reserve(data.size());
+        for (auto &v : data) {
+            msg.data.push_back(static_cast<int32_t>(v));
+        }
+        publisher_->publish(msg);
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr publisher_;
+};
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+
+    rclcpp::executors::SingleThreadedExecutor exec;
+    auto ps4_listener = std::make_shared<PS4_Listener>();
+    exec.add_node(ps4_listener);
+    exec.spin();
+
+    rclcpp::shutdown();
+    return 0;
+}
