d323d623b8a21fdef74fedc218d84ebf726dbd67
mo-ri-tania <i.morimoto.05150@gmail.com>
2025-08-20 04:14:00 +0900

    RθZ実装(動作確認済み)

--- ros2udp_apollo4/src/apollo4_main.cpp
+++ ros2udp_apollo4/src/apollo4_main.cpp
@@ -25,33 +25,35 @@ RRST-NHK-Project 2025
 #define MC_PRINTF 0 // マイコン側のprintfを無効化・有効化(0 or 1)
 
 // L2,R2のデッドゾーン
-#define DEADZONE_L2 0.3
-#define DEADZONE_R2 0.3
+#define DEADZONE_L2 0.5
+#define DEADZONE_R2 0.5
 
-//R1,TRIANGLEの切り替えに必要
+//R1,R2,TRIANGLEの切り替えに必要
 int R1_count = 0;           // R1が押された回数
+int R2_count = 0;           // R2が押された回数
 int TRIANGLE_count = 0;     // TRIANGLEが押された回数
 bool prev_R1 = false;       // 前回のR1の状態
+bool prev_R2 = false;       // 前回のR1の状態
 bool prev_TRIANGLE = false;       // 前回のTRIANGLEの状態
 
 //モード切り替え
 bool CHANGEMODE = false;
 
 // 速度
-int wheelspeed = 25;
+int wheelspeed = 50;
 int absorbspeed = 30;
 
 //サーボ
 int servo_angle = 90;        // 現在の角度（初期は90度）
-int servo_step = 1;          // 1回の更新で動く角度
+int servo_step = 2;          // 1回の更新で動く角度
 
 // サーボの組み付け時のズレを補正（度数法）
-int SERVO1_CAL = 10;
-int SERVO2_CAL = 8;
-int SERVO3_CAL = 23;
-int SERVO4_CAL = 20;
-int SERVO5_CAL = 10;
-int SERVO6_CAL = 10;
+int SERVO1_CAL = 0;
+int SERVO2_CAL = 0;
+int SERVO3_CAL = 0;
+int SERVO4_CAL = 0;
+int SERVO5_CAL = 0;
+int SERVO6_CAL = 0;
 
 
 std::vector<int16_t> data(19, 0); // マイコンに送信される配列"data"
@@ -90,7 +92,7 @@ public:
     PS4_Listener(const std::string &ip, int port)
         : Node("nhk25_mr_sd"), udp_(ip, port) {
         subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
-            "joy0", 10,
+            "joy", 10,
             std::bind(&PS4_Listener::ps4_listener_callback, this,
                       std::placeholders::_1));
         RCLCPP_INFO(this->get_logger(),
@@ -122,16 +124,19 @@ private:
         float L2 = (-1 * msg->axes[2] + 1) / 2;
         float R2 = (-1 * msg->axes[5] + 1) / 2;
 
-        bool SHARE = msg->buttons[8];
-        bool OPTION = msg->buttons[9];
+        //bool SHARE = msg->buttons[8];
+        //bool OPTION = msg->buttons[9];
         bool PS = msg->buttons[10];
 
         // bool L3 = msg->buttons[11];
-        bool R3 = msg->buttons[12];
+        //bool R3 = msg->buttons[12];
 
-        //L2でゼロイチ制御するためdeadzone追加
+        //L2,R2でゼロイチ制御するためdeadzone追加
         if(L2 <= DEADZONE_L2){
             L2 = 0; 
+        }
+        if(R1 <= DEADZONE_R2){
+            R2 = 0; 
         }
         // 前回の状態を保持する static 変数
         static bool last_PS = false;
@@ -160,30 +165,84 @@ private:
         // RθZの操作!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // R軸の操作
         if (UP) {       // 正転
-            data[1] = -wheelspeed;
-        }
-        if (DOWN) {     // 逆転
             data[1] = wheelspeed;
+        }else if (DOWN) {     // 逆転
+            data[1] = -wheelspeed;
+        }else{
+            data[1] = 0;
         }
-        // θ軸の操作
+        //θ軸の操作
         if (LEFT) {     // 正転
-            data[2] = -wheelspeed;
-        }
-        if (RIGHT) {    // 逆転
             data[2] = wheelspeed;
+        }else if (RIGHT) {    // 逆転
+            data[2] = -wheelspeed;
+        }else{
+            data[2] = 0;
         }
         // Z軸の操作
         if(L1) {         // 正転
-            data[3] = -wheelspeed; 
-        }
-        if(L2>0) {       // 逆転
-            data[3] = wheelspeed;
+            data[6] = -wheelspeed; 
+        }else if(L2>0) {       // 逆転
+            data[6] = wheelspeed;
+        }else{
+            data[6] = 0;
         }
 
         // 展開指変更!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //R2順方向
+        if(R2>0){   //ゼロイチ判断
+            R2 = 1;
+        }
+        // 0 → 1 に変化したときだけカウントしR2_countが加算されていく
+        if (R2 && !prev_R2) {   // 論理積(AND演算子よりtrue&&trueのときのみtrueとなりそれ以外はfalse)
+            R2_count++;
+        }
+        if(R2_count %6 == 0) {  // 垂直の状態
+            data[9] = 90 + SERVO1_CAL;
+            data[10] = 90 + SERVO2_CAL;
+            data[11] = 90 + SERVO3_CAL;
+            data[12] = 90 + SERVO4_CAL;
+            data[13] = 90 + SERVO5_CAL;
+        }
+        if(R2_count %6 == 1) {  // 取る状態1
+            data[9] = 45 + SERVO1_CAL;
+            data[10] = 100 + SERVO2_CAL;
+            data[11] = 100 + SERVO3_CAL;
+            data[12] = 100 + SERVO4_CAL;
+            data[13] = 100 + SERVO5_CAL;
+        }
+        if(R2_count %6 == 2) {  // 取る状態2
+            data[9] = 100 + SERVO1_CAL;
+            data[10] = 45 + SERVO2_CAL;
+            data[11] = 100 + SERVO3_CAL;
+            data[12] = 100 + SERVO4_CAL;
+            data[13] = 100 + SERVO5_CAL;
+        }
+        if(R2_count %6 == 3) {  // 取る状態3
+            data[9] = 100 + SERVO1_CAL;
+            data[10] = 100 + SERVO2_CAL;
+            data[11] = 45 + SERVO3_CAL;
+            data[12] = 100 + SERVO4_CAL;
+            data[13] = 100 + SERVO5_CAL;
+        }
+        if(R2_count %6 == 4){   // 取る状態4
+            data[9] = 100 + SERVO1_CAL;
+            data[10] = 100 + SERVO2_CAL;
+            data[11] = 100 + SERVO3_CAL;
+            data[12] = 45 + SERVO4_CAL;
+            data[13] = 100 + SERVO5_CAL;
+        }
+        if(R2_count %6 == 5){   // 取る状態5
+            data[9] = 100 + SERVO1_CAL;
+            data[10] = 100 + SERVO2_CAL;
+            data[11] = 100 + SERVO3_CAL;
+            data[12] = 100 + SERVO4_CAL;
+            data[13] = 45 + SERVO5_CAL;
+        }
+        //R1逆方向
         // 0 → 1 に変化したときだけカウントしR1_countが加算されていく
         if (R1 && !prev_R1) {   // 論理積(AND演算子よりtrue&&trueのときのみtrueとなりそれ以外はfalse)
-            R1_count++;
+            R1_count = R1_count + 5;
         }
         if(R1_count %6 == 0) {  // 垂直の状態
             data[9] = 90 + SERVO1_CAL;
@@ -245,7 +304,7 @@ private:
             }
         }
         // サーボ指令
-        data[15] = servo_angle + SERVO6_CAL;
+        data[14] = servo_angle + SERVO6_CAL;
 
         //吸着モード切り替え!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // 0 → 1 に変化したときだけカウントしTRIANGLE_countが加算されていく
@@ -287,17 +346,13 @@ private:
 
         // 状態を更新
         prev_R1 = R1; 
+        prev_R2 = R2;
         prev_TRIANGLE = TRIANGLE;
 
 
 
         // デバッグ用（for文でcoutするとカクつく）
-        // std::cout << data[0] << ", " << data[1] << ", " << data[2] << ", " << data[3] << ", ";
-        // std::cout << data[4] << ", " << data[5] << ", " << data[6] << ", " << data[7] << ", ";
-        // std::cout << data[8] << ", " << data[9] << ", " << data[10] << ", " << data[11] << ", ";
-        // std::cout << data[12] << ", " << data[13] << ", " << data[14] << ", " << data[15] << ", ";
-        // std::cout << data[16] << ", " << data[17] << ", " << data[18] << std::endl;
-        std::cout << data[4] << ", " << data[5] << std::endl; //吸着できているか確認する用
+        //std::cout << data[4] << ", " << data[5] << std::endl; //吸着できているか確認する用
 
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
@@ -308,31 +363,7 @@ private:
     UDP udp_;
 };
 
-class Servo_Deg_Publisher : public rclcpp::Node {
-public:
-    Servo_Deg_Publisher() : Node("mr_servo_deg_publisher") {
-        // Publisherの作成
-        publisher_ = this->create_publisher<std_msgs::msg::Int32MultiArray>(
-            "mr_servo_deg", 10);
 
-        // タイマーを使って定期的にメッセージをpublish
-        timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(10),
-            std::bind(&Servo_Deg_Publisher::publish_message, this));
-    }
-
-private:
-    void publish_message() {
-        auto message = std_msgs::msg::Int32MultiArray();
-        message.data = {data[9], data[10], data[11], data[12], data[13], data[14]};
-
-        // RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.data);
-        publisher_->publish(message); // メッセージをpublish
-    }
-
-    rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
-};
 
 class Params_Listener : public rclcpp::Node {
 public:
@@ -363,7 +394,7 @@ int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
 
     // figletでノード名を表示
-    std::string figletout = "figlet MR SwerveDrive";
+    std::string figletout = "figlet APOLLO4MAIN";
     int result = std::system(figletout.c_str());
     if (result != 0) {
         std::cerr << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -376,11 +407,9 @@ int main(int argc, char *argv[]) {
     }
 
     rclcpp::executors::MultiThreadedExecutor exec;
-    auto ps4_listener = std::make_shared<PS4_Listener>(IP_MR_SD, PORT_MR_SD);
-    auto servo_deg_publisher = std::make_shared<Servo_Deg_Publisher>();
+    auto ps4_listener = std::make_shared<PS4_Listener>(IP_AP4_MAIN, PORT_AP4_MAIN);
     auto params_listener = std::make_shared<Params_Listener>();
     exec.add_node(ps4_listener);
-    exec.add_node(servo_deg_publisher);
     exec.add_node(params_listener);
 
     exec.spin();
