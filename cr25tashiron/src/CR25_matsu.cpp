cd14e7bfded63642e39b588c49eb149dc5675df5
takashinosuke <tiramisu-chaohan.8989@docomo.ne.jp>
2025-08-28 19:34:48 +0900

    colcon通りました

--- cr25tashiron/src/CR25_matsu.cpp
+++ cr25tashiron/src/CR25_matsu.cpp
@@ -13,35 +13,33 @@ PS4コントローラーの入力を取得するサンプルプログラム
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "std_msgs/msg/int32_multi_array.hpp"
+#include "actuator_msg/msg/actuator_msg.hpp"
+/*メッセージの定義
+# 共通
+int32 actuator_id          # アクチュエータの種別ごと（例：MD1とSERVO1は共存可能）
+int32 actuator_type        # アクチュエータ種別 (0:デバッグ, 1:モタドラ, 2:サーボ, 3:ソレノイド)
+string actuator_type_name  # "MD", "SERVO", "SV"
+bool enable                # 有効・無効
+
+# --- モタドラ用 ---
+int32 motor_duty           # [%]
+int32 motor_target_rpm     # [RPM]
+int32 motor_target_pos     #
+int32 motor_target_torque  #
+
+# --- サーボ用 ---
+int32 servo_angle_degree   # [deg]
+int32 servo_speed          #
+
+# --- ソレノイドバルブ用 ---
+bool solenoid_state        # [True/False]
+*/
+
 
 #define MC_PRINTF 0 // マイコン側のprintfを無効化・有効化(0 or 1)
 
+
 std::vector<int32_t> data(19, 0); // マイコンに送信される配列"data"
-/*
-マイコンに送信される配列"data"
-debug: マイコンのprintfを有効化, MD: モータードライバー, TR: トランジスタ
-| data[n] | 詳細 | 範囲 |
-| ---- | ---- | ---- |
-| data[0] | debug | 0 or 1 |
-| data[1] | MD1 | -100 ~ 100 |
-| data[2] | MD2 | -100 ~ 100 |
-| data[3] | MD3 | -100 ~ 100 |
-| data[4] | MD4 | -100 ~ 100 |
-| data[5] | MD5 | -100 ~ 100 |
-| data[6] | MD6 | -100 ~ 100 |
-| data[7] | Servo1 | 0 ~ 270 |
-| data[8] | Servo2 | 0 ~ 270 |
-| data[9] | Servo3 | 0 ~ 270 |
-| data[10] | Servo4 | 0 ~ 270 |
-| data[11] | TR1 | 0 or 1|
-| data[12] | TR2 | 0 or 1|
-| data[13] | TR3 | 0 or 1|
-| data[14] | TR4 | 0 or 1|
-| data[15] | TR5 | 0 or 1|
-| data[16] | TR6 | 0 or 1|
-| data[17] | TR7 | 0 or 1|
-| data[18] | TR8 | 0 or 1|
-*/
 
 class PS4_Listener : public rclcpp::Node {
 public:
@@ -96,8 +94,8 @@ private:
         if (CIRCLE) {
             std::cout << "CIRCLE" << std::endl;
             data[2] = 1;
-            publish_data();
-        }
+         publish_data();
+         }
 
         //data[3] = R2 * 255;
         publish_data();
@@ -117,6 +115,8 @@ private:
     rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr publisher_;
 };
 
+
+
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
 
@@ -125,7 +125,7 @@ int main(int argc, char *argv[]) {
     auto node = rclcpp::Node::make_shared("actuator_publisher");
     auto pub = node->create_publisher<actuator_msg::msg::ActuatorMsg>("actuator_cmd", 10);
     rclcpp::Rate rate(10);
-
+    
     exec.add_node(ps4_listener);
     exec.spin();
 
@@ -144,7 +144,6 @@ int main(int argc, char *argv[]) {
         pub->publish(msg);
         rate.sleep();
     }
-
     rclcpp::shutdown();
     return 0;
 }
