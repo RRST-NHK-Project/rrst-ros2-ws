971a0d2ff8d8af5e786b1557d06b227e7df063c9
takashinosuke <tiramisu-chaohan.8989@docomo.ne.jp>
2025-09-12 14:28:32 +0900

    スイッチ用のノード追加

---
+++ cr25tashiron/src/CR25_matsu_sw.cpp
@@ -0,0 +1,85 @@
+/*
+RRST-NHK-Project 2025
+PS4コントローラーの入力を取得するサンプルプログラム
+esp32マイコンにアクチュエータ指令を送るサンプルプログラム
+*/
+
+// 標準
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
+// ROS
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
+#include "actuator_msg/msg/actuator_msg.hpp"
+#include <vector>
+
+#define MC_PRINTF 0 // マイコン側のprintfを無効化・有効化(0 or 1)
+
+
+std::vector<int16_t> data(25, 0); // マイコンに送信される配列"data"
+
+class Angle_Listener : public rclcpp::Node {
+public:
+    Angle_Listener()
+        : Node("angle_listener") {
+
+        subscription_ = this->create_subscription<std_msgs::msg::Int32MultiArray>(
+            "from_esp32", 10,
+            std::bind(&Angele_Listener::angle_listener_callback, this,
+                      std::placeholders::_1));
+
+        publisher_ = this->create_publisher<std_msgs::msg::Int32MultiArray>("to_esp32_0", 10);
+
+        RCLCPP_INFO(this->get_logger(),
+                    "Angle Listener Node has been started.");
+    }
+
+
+private:   
+    void angle_listener_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
+        int sw1 = msg->data[0]; // ボタンの状態を取得
+        int angle1 = msg->data[8]; // 現在の角度を取得
+       
+         // スイッチが 0→1 になった瞬間にゼロリセット
+        if (sw1 == 1 && last_sw_1 == 0) {
+            zero_offset_1 = angle1;
+            zero_set_1 = true;
+            RCLCPP_INFO(this->get_logger(), "Zero set at angle=%d", angle1);
+        }
+        last_sw_1 = sw;
+
+        int corrected_angle1 = angle1;
+        if (zero_set_) {
+            corrected_angle1 = angle1 - zero_offset_1;
+        }
+        
+        auto msg = std_msgs::msg::Int32MultiArray();
+        out_msg.data = msg->data;//元データコピー
+        out_msg.data[8] = corrected_angle1;
+        publisher_->publish(out_msg);
+    }
+    rclcpp::Subscription<std_msgs::msg::Int32MultiArray>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr publisher
+
+    int last_sw_1 = 0;
+    int zero_offset_1 = 0;
+    bool zero_set_1 = false;
+
+}
+
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+
+    rclcpp::executors::SingleThreadedExecutor exec;
+    auto angle_listener = std::make_shared<Angle_Listener>();
+    exec.add_node(angle_listener);
+    exec.spin();
+
+    rclcpp::shutdown();
+    return 0;
+}
