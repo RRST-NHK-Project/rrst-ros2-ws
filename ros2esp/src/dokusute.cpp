e66a06cdd8f1eb788b764dd9043b195e34dc27fa
kokiyoshiura <koki2022@gmail.com>
2026-01-29 13:39:25 +0900

    test

--- ros2esp/src/dokusute.cpp
+++ ros2esp/src/dokusute.cpp
@@ -13,7 +13,64 @@
 // 定数・変数
 #define MC_PRINTF 0 // マイコン側のprintfを無効化・有効化(0 or 1)
 
+
+// スティックのデッドゾーン
+#define DEADZONE_L 0.3
+#define DEADZONE_R 0.3
+
+// 定数k
+#define k 0.05
+
+// PIDパラメータ（チューニングが必要）
+#define speed_Kp 0.3  // 速度Pゲイン
+#define speed_Ki 0.05 // 速度Iゲイン
+#define speed_Kd 0.05 // 速度Dゲイン
+
+#define speed_limit 30
+#define deg_limit 360
+#define DPAD_SPEED 30  
+
+bool AGRESSIVEMODE = false; // 暴走モードの初期値0
+bool LATERALMOTIONMODE = false;
+
+// グローバル変数（角度一覧）
+int deg = 0;
+int previous_deg = 0;
+int truedeg = 0;
+int desired_deg = 0;
+int measured_deg = 0;
+
+// PID用の内部変数
+double speed_Error = 0.0;
+double speed_last_Error = 0.0;
+double speed_Integral = 0.0;
+double speed_Differential = 0.0;
+double speed_Output = 0.0;
+
+// 制御周期 [秒]
+const double dt = 0.05; // 50ms
+
+// 速度
+int wheelspeed = 50;
+int yawspeed = 10;
+int previous_speed = 0;
+int desired_speed = 30;
+int measured_speed = 0;
+int LATERALMOTION_speed = 15;
+// static double current_motor_command = 0.0;
+
+// サーボの組み付け時のズレを補正（度数法）
+int SERVO1_CAL = 28;
+int SERVO2_CAL = -16;
+int SERVO3_CAL = 10;
+int SERVO4_CAL = 20;
+
+// 最近傍点距離の格納
+float min_distance = 0;
+bool front_cleared = false;
+
 std::vector<int16_t> data(18, 0); // マイコンに送信される配列"data"
+
 
 class PS4_Listener : public rclcpp::Node
 {
@@ -38,59 +95,221 @@ private:
     {
 
         // コントローラーの入力を取得、使わない入力はコメントアウト推奨
-        //  float LS_X = -1 * msg->axes[0];
-        //  float LS_Y = msg->axes[1];
-        //  float RS_X = -1 * msg->axes[3];
-        //  float RS_Y = msg->axes[4];
+        
+        data[0] = MC_PRINTF; // マイコン側のprintfを無効化・有効化(0 or 1)
+
+        float LS_X = -1 * msg->axes[0];
+        float LS_Y = msg->axes[1];
+        float RS_X = -1 * msg->axes[3];
+        // float RS_Y = msg->axes[4];
 
         // bool CROSS = msg->buttons[0];
-        // bool CIRCLE = msg->buttons[1];
-        // bool TRIANGLE = msg->buttons[2];
-        //bool SQUARE = msg->buttons[3];
+        //  bool CIRCLE = msg->buttons[1];
+        //  bool TRIANGLE = msg->buttons[2];
+        //  bool SQUARE = msg->buttons[3];
 
-        // bool LEFT = msg->axes[6] == 1.0;
-        // bool RIGHT = msg->axes[6] == -1.0;
-        // bool UP = msg->axes[7] == 1.0;
-        // bool DOWN = msg->axes[7] == -1.0;
+        bool LEFT = msg->axes[6] == 1.0;
+        bool RIGHT = msg->axes[6] == -1.0;
+        bool UP = msg->axes[7] == 1.0;
+        bool DOWN = msg->axes[7] == -1.0;
 
         bool L1 = msg->buttons[4];
-        bool R1 = msg->buttons[5];
+        // bool R1 = msg->buttons[5];
 
-        // float L2_DIGITAL = (-1 * msg->axes[2] + 1) / 2;
-        // float R2_DIGITAL = (-1 * msg->axes[5] + 1) / 2;
+        // float L2 = (-1 * msg->axes[2] + 1) / 2;
+        float R2 = (-1 * msg->axes[5] + 1) / 2;
 
-        // bool L2 = msg->buttons[6];
-        // bool R2 = msg->buttons[7];
+        // bool SHARE = msg->buttons[8];
+        bool OPTION = msg->buttons[9];
+        // bool PS = msg->buttons[10];
 
-        //bool SHARE = msg->buttons[8];
-        // bool OPTION = msg->buttons[9];
-       // bool PS = msg->buttons[10];
+        // bool L3 = msg->buttons[11];
+        bool R3 = msg->buttons[12];
+
+        static bool last_option = false; // 前回の状態を保持する static 変数
+        // OPTION のラッチ状態を保持する static 変数（初期状態は OFF とする）
+        static bool option_latch = false;
+
+        static bool last_R3 = false;
+        static bool R3_latch = false;
+
+        if (OPTION && !last_option) {
+            option_latch = !option_latch;
+        }
+        if (R3 && !last_R3) {
+            R3_latch = !R3_latch;
+        }
+
+        last_option = OPTION;
+        AGRESSIVEMODE = option_latch;
+        last_R3 = R3;
+        LATERALMOTIONMODE = R3_latch;
+
+        if (AGRESSIVEMODE == 0) {
+            wheelspeed = 75;
+            data[11] = 1; // LED光らない
+        }
+        if (AGRESSIVEMODE == 1) {
+            wheelspeed = 50;
+            data[11] = 0; // LED光る
+        }
 
         // bool L3 = msg->buttons[11];
         // bool R3 = msg->buttons[12];
 
-        // static bool last_circle = false; // 前回の状態を保持する static 変数
-        // static bool circle_latch = false;
+        // PSボタンで緊急停止 TODO:復帰機能の実装
+        // if (PS && !ps_prev) {
+        //     ps_state = !ps_state; // トグル切り替え
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        // }
+        // ps_prev = PS;       // 現在のPSボタンの状態を保存
+        // soft_es = ps_state; // 緊急停止状態を保存
 
-        //static bool last_triangle = false;
+        // if (soft_es) {
+        //     data[0] = -1;
+        //     std::cout << "緊急停止！" << std::endl;
+        // } else if (MC_PRINTF == 0 || MC_PRINTF == 1) {
+        //     data[0] = MC_PRINTF;
+        // } else {
+        //     data[0] = 1;
+        // }
 
-        data[0] = MC_PRINTF; // マイコン側のprintfを無効化・有効化(0 or 1)
+        // if (L1) {
+        //     Automation::automatic_cruise(udp_);
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // }
 
-        data[1] = 30;
-
-        static int angle = 0 ;
-        int move = 5 ;
-        if(L1 && !R1){
-            angle = angle + move ;
-            if (angle>=270) angle = 270 ;
+        float rad = atan2(LS_Y, LS_X);
+        deg = rad * 180 / M_PI;
+        // XY座標での正しい角度truedeg
+        truedeg = deg;
+        if ((0 <= truedeg) && (truedeg <= 180)) {
+            truedeg = truedeg;
         }
-        
-        if(R1 && !L1){
-            angle = angle - move  ;
-            if (angle<=0) angle = 0 ;
+        if ((-180 <= truedeg) && (truedeg <= 0)) {
+            truedeg = -truedeg + 360;
         }
 
-        data[5] = angle ;
+        // ！！！！！最重要！！！！！
+        //  XY座標での９０度の位置に１３５度を変換して計算
+        if ((-180 <= deg) && (deg <= -135)) {
+            deg = -deg - 135;
+        } else {
+            deg = 225 - deg;
+        }
+
+        // deadzone追加
+        if ((fabs(LS_X) <= DEADZONE_R) && (fabs(LS_Y) <= DEADZONE_R) && (fabs(RS_X) <= DEADZONE_L)) {
+            deg = 135;
+            data[1] = 0;
+            data[2] = 0;
+            data[3] = 0;
+            data[4] = 0;
+            data[5] = deg + SERVO1_CAL;
+            data[8] = deg + SERVO2_CAL;
+            data[9] = deg + SERVO3_CAL;
+            data[10] = deg + SERVO4_CAL;
+        }
+
+        data[1] = -wheelspeed * R2;
+        data[2] = -wheelspeed * R2;
+        data[3] = -wheelspeed * R2;
+        data[4] = -wheelspeed * R2;
+        data[5] = deg + SERVO1_CAL;
+        data[8] = deg + SERVO2_CAL;
+        data[9] = deg + SERVO3_CAL;
+        data[10] = deg + SERVO4_CAL;
+
+        if (LEFT) {
+            deg = 45;
+            data[1] = -wheelspeed * R2;
+            data[2] = -wheelspeed * R2;
+            data[3] = -wheelspeed * R2;
+            data[4] = -wheelspeed * R2;
+        }
+        if (RIGHT) {
+            deg = 45;
+            data[1] = wheelspeed * R2;
+            data[2] = wheelspeed * R2;
+            data[3] = wheelspeed * R2;
+            data[4] = wheelspeed * R2;
+        }
+        if (UP) {
+            deg = 135;
+            data[1] = -wheelspeed * R2;
+            data[2] = -wheelspeed * R2;
+            data[3] = -wheelspeed * R2;
+            data[4] = -wheelspeed * R2;
+        }
+        if (DOWN) {
+            deg = 135;
+            data[1] = wheelspeed * R2;
+            data[2] = wheelspeed * R2;
+            data[3] = wheelspeed * R2;
+            data[4] = wheelspeed * R2;
+        }
+
+        // 独ステが扱えない範囲の変換
+        if ((270 < deg) && (deg < 360)) {
+            deg = deg - 180;
+            data[1] = wheelspeed * R2;
+            data[2] = wheelspeed * R2;
+            data[3] = wheelspeed * R2;
+            data[4] = wheelspeed * R2;
+            data[5] = deg + SERVO1_CAL;
+            data[8] = deg + SERVO2_CAL;
+            data[9] = deg + SERVO3_CAL;
+            data[10] = deg + SERVO4_CAL;
+        }
+
+        // 角度だけ横移動
+        if (R3_latch == 0) {
+                data[5] = deg + SERVO1_CAL;
+                data[8] = deg + SERVO2_CAL;
+                data[9] = deg + SERVO3_CAL;
+                data[10] = deg + SERVO4_CAL;
+        }
+        if (R3_latch == 1) {
+                data[5] = 45 + SERVO1_CAL;
+                data[8] = 45 + SERVO2_CAL;
+                data[9] = 45 + SERVO3_CAL;
+                data[10] = 45 + SERVO4_CAL;
+            if (LEFT) {
+                data[1] = -LATERALMOTION_speed;
+                data[2] = -LATERALMOTION_speed ;
+                data[3] = -LATERALMOTION_speed ;
+                data[4] = -LATERALMOTION_speed ;
+            }
+            if (RIGHT) {
+                data[1] = LATERALMOTION_speed ;
+                data[2] = LATERALMOTION_speed ;
+                data[3] = LATERALMOTION_speed ;
+                data[4] = LATERALMOTION_speed ;
+            }
+        }
+
+        // 時計回りYAW回転
+        if (RS_X > 0 && fabs(RS_X) >= DEADZONE_R) {
+            data[5] = 180 + SERVO1_CAL;
+            data[8] = 90 + SERVO2_CAL;
+            data[9] = 90 + SERVO3_CAL;
+            data[10] = 180 + SERVO4_CAL;
+            data[1] = -yawspeed;
+            data[2] = yawspeed;
+            data[3] = -yawspeed;
+            data[4] = yawspeed;
+        }
+        // 半時計回りYAW回転
+        if (0 > RS_X && fabs(RS_X) >= DEADZONE_R) {
+            data[5] = 180 + SERVO1_CAL;
+            data[8] = 90 + SERVO2_CAL;
+            data[9] = 90 + SERVO3_CAL;
+            data[10] = 180 + SERVO4_CAL;
+            data[1] = yawspeed;
+            data[2] = -yawspeed;
+            data[3] = yawspeed;
+            data[4] = -yawspeed;
+        }
 
         // デバッグ用（for文でcoutするとカクつく）
         std::cout << data[0] << ", " << data[1] << ", " << data[2] << ", " << data[3] << ", ";
