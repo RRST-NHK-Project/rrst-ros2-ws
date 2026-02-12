33a451cba429ff4385241286765ceeda277a8925
K.Tashiro <tashikou1682@gmail.com>
2026-02-13 03:29:05 +0900

    R1機構（マガジン）実装途中

--- nr26_r1_hw_ctrl/src/NR26_pick.cpp
+++ nr26_r1_hw_ctrl/src/NR26_pick.cpp
@@ -26,13 +26,11 @@ Copyright (c) 2025 RRST-NHK-Project. All rights reserved.
 #define DEADZONE_L 0.3
 #define DEADZONE_R 0.3
 
-class HardWareControl : public rclcpp::Node
-{
+class HardWareControl : public rclcpp::Node {
 public:
     HardWareControl(uint8_t device_id)
         : Node("hardware_control_" + std::to_string(device_id)),
-          device_id_(device_id)
-    {
+          device_id_(device_id) {
 
         // 配列を0で初期化
         data_.assign(TX16NUM, 0);
@@ -94,8 +92,7 @@ public:
     }
 
 private:
-    void ps4_listener_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
-    {
+    void ps4_listener_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
 
         // コントローラーの入力を取得、使わない入力はコメントアウト推奨
         // float LS_X = -1 * msg->axes[0];
@@ -106,10 +103,10 @@ private:
         bool CROSS = msg->buttons[0];
         bool CIRCLE = msg->buttons[1];
         // bool TRIANGLE = msg->buttons[2];
-        // bool SQUARE = msg->buttons[3];
+        bool SQUARE = msg->buttons[3];
 
-        // bool LEFT = msg->axes[6] == 1.0;
-        // bool RIGHT = msg->axes[6] == -1.0;
+        bool LEFT = msg->axes[6] == 1.0;
+        bool RIGHT = msg->axes[6] == -1.0;
         bool UP = msg->axes[7] == 1.0;
         bool DOWN = msg->axes[7] == -1.0;
 
@@ -140,33 +137,32 @@ private:
         // CROSS:「マガジン回転」
         // ボタンを一回押すごとにサーボモーターを45°づつ回転する
         // =================================================================
-        data_[1] = CROSS;
-        static int MAG_SERVO_ANGLE[] = {0, 45, 90, 135};
-        static int cross_pre = 0;
-        static int s = 0;
-        static int CROSS_PUSH_MAX = 4;
 
-        if (CROSS == 1 && cross_pre == 0)
-        {
-            s = (s + 1) % CROSS_PUSH_MAX;
-        }
-        if (s == 0)
-        {
-            data_[9] = MAG_SERVO_ANGLE[0];
-        }
-        if (s == 1)
-        {
-            data_[9] = MAG_SERVO_ANGLE[1];
-        }
-        if (s == 2)
-        {
-            data_[9] = MAG_SERVO_ANGLE[2];
-        }
-        if (s == 3)
-        {
-            data_[9] = MAG_SERVO_ANGLE[3];
-        }
-        cross_pre = CROSS;
+        // data_[1] = CROSS;
+        // static int MAG_SERVO_ANGLE[] = {270, 225, 190, 135, 0};
+        // static int cross_pre = 0;
+        // static int s = 0;
+        // static int CROSS_PUSH_MAX = 5;
+
+        // if (CROSS == 1 && cross_pre == 0) {
+        //     s = (s + 1) % CROSS_PUSH_MAX;
+        // }
+        // if (s == 0) {
+        //     data_[9] = MAG_SERVO_ANGLE[0];
+        // }
+        // if (s == 1) {
+        //     data_[9] = MAG_SERVO_ANGLE[1];
+        // }
+        // if (s == 2) {
+        //     data_[9] = MAG_SERVO_ANGLE[2];
+        // }
+        // if (s == 3) {
+        //     data_[9] = MAG_SERVO_ANGLE[3];
+        // }
+        // if (s == 4) {
+        //     data_[9] = MAG_SERVO_ANGLE[4];
+        // }
+        // cross_pre = CROSS;
 
         // =================================================================
         // CIRCLE:「棒ホールド機構」
@@ -176,41 +172,60 @@ private:
         // angle = 10のとき最下部までお仕込み
         // angle = 245のときマガジンに戻してる
 
-        static int BAR_HOLD_ANGLE = 45;
-        static int BAR_RELE_ANGLE = 225;
+        static int BAR_PUSH_ANGLE = 10;
+        static int BAR_HOLD_ANGLE = 43;
+        static int BAR_RELE_ANGLE = 245;
         static int circle_pre = 0;
         static int t = 0;
-        static int CIRCLE_PUSH_MAX = 2;
+        static int CIRCLE_PUSH_MAX = 3;
 
-        if (CIRCLE == 1 && circle_pre == 0)
-        {
+        if (CIRCLE == 1 && circle_pre == 0) {
             t = (t + 1) % CIRCLE_PUSH_MAX;
         }
-        if (t == 0)
-        {
+        if (t == 0) {
             data_[10] = BAR_RELE_ANGLE;
         }
-        if (t == 1)
-        {
+        if (t == 1) {
             data_[10] = BAR_HOLD_ANGLE;
+        }
+        if (t == 2) {
+            data_[10] = BAR_PUSH_ANGLE;
         }
 
         circle_pre = CIRCLE;
+
+        // =================================================================
+        // SQUARE:「棒ロック」
+        // ボタンを一回押すごとに2つのサーボモーターの角度状態を同時に変化させる
+        // =================================================================
+
+        static int BAR_BTM_HOLD_ANGLE = 0;
+        static int BAR_BTM_ANGLE = 150;
+        static int square_pre = 0;
+        static int u = 0;
+        static int SQUARE_PUSH_MAX = 2;
+
+        if (SQUARE == 1 && square_pre == 0) {
+            u = (u + 1) % SQUARE_PUSH_MAX;
+        }
+        if (u == 0) {
+            data_[11] = BAR_BTM_ANGLE;
+        }
+        if (u == 1) {
+            data_[11] = BAR_BTM_HOLD_ANGLE;
+        }
+
+        square_pre = SQUARE;
         // =================================================================
         // UP:「槍押上機構」
         // ボタンを押し続けると槍を押し上げるモーターが回転し続ける
         // =================================================================
         // data_[3] = UP;
-        if (UP)
-        {
+        if (UP) {
             data_[1] = 50;
-        }
-        else if (DOWN)
-        {
+        } else if (DOWN) {
             data_[1] = -50;
-        }
-        else
-        {
+        } else {
             data_[1] = 0;
         }
         // =================================================================
@@ -223,6 +238,26 @@ private:
         //     data_[1] = 0;
         // }
 
+        // =================================================================
+        // LR
+        // マガジン調整用
+        static int MAG_ADJ_STEP = 5;
+        static int left_pre = 0;
+        static int right_pre = 0;
+        static int mag_servo_angle = 270;
+
+        if (LEFT == 1 && left_pre == 0) {
+            mag_servo_angle -= MAG_ADJ_STEP;
+            data_[9] = mag_servo_angle;
+        }
+        left_pre = LEFT;
+        if (RIGHT == 1 && right_pre == 0) {
+            mag_servo_angle += MAG_ADJ_STEP;
+            data_[9] = mag_servo_angle;
+        }
+        right_pre = RIGHT;
+        // =================================================================
+
         // デバッグ用
         RCLCPP_INFO(
             get_logger(),
@@ -234,8 +269,7 @@ private:
     }
 
     // publish
-    void publisher_timer_callback()
-    {
+    void publisher_timer_callback() {
         std_msgs::msg::Int16MultiArray msg;
 
         msg.data = data_;
@@ -245,11 +279,9 @@ private:
 
     void
     sensor_callback(
-        const std_msgs::msg::Int16MultiArray::SharedPtr msg)
-    {
+        const std_msgs::msg::Int16MultiArray::SharedPtr msg) {
         // 最低限：サイズチェック
-        if (msg->data.size() < RX16NUM)
-        {
+        if (msg->data.size() < RX16NUM) {
             RCLCPP_WARN(this->get_logger(),
                         "serial_rx_%d: data too short (%zu)",
                         device_id_, msg->data.size());
@@ -289,15 +321,13 @@ private:
     std::vector<int16_t> data_;
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
 
     // figletでノード名を表示
     std::string figletout = "figlet Serial Bridge Host";
     int result = std::system(figletout.c_str());
-    if (result != 0)
-    {
+    if (result != 0) {
         std::cerr << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
                   << std::endl;
         std::cerr << "Please install 'figlet' with the following command:"
