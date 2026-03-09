62af47dd28c9ddf9fb3758cb77094d93ae41892a
K.Tashiro <tashikou1682@gmail.com>
2026-03-09 11:27:25 +0900

    R1, R2のlaunch追加

---
+++ nr26_r2_hw_ctrl/launch/bringup.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_node',
+        output='screen'
+    )
+
+    r2_md = Node(
+        package='nr26_r2_hw_ctrl',
+        executable='r2_md',
+        name='r2_md',
+        output='screen'
+    )
+
+    r2_mc = Node(
+        package='nr26_r2_hw_ctrl',
+        executable='r2_mc',
+        name='r2_mc',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        joy_node,
+        r2_md,
+        r2_mc
+    ])
