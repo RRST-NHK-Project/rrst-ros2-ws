c3d45b3a82ca0040eea0badf47908e61689aafaf
kokiyoshiura <koki2022@gmail.com>
2026-03-18 14:42:07 +0900

    ArUkoマーカーでの相対位置推定用コード（仮）　まだ未完成です

---
+++ aruco_tracker/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'aruco_tracker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='koki2022@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'aruco_test = aruco_tracker.aruco_test:main',
+        'aruco_generator = aruco_tracker.aruco_generator:main',
+        'camera = aruco_tracker.camera:main',
+        'camera_calibration = aruco_tracker.camera_calibration:main',
+        ],
+    },
+)
