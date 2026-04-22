e17c2fdcd0185b49093dcfe403b08683f0787e00
taiyouss <haru0517fujisawa@icloud.com>
2026-04-22 17:32:03 +0900

    帰りたい。。。

--- nr26_r1_hw_ctrl/src/R1_dev.cpp
+++ nr26_r1_hw_ctrl/src/R1_dev.cpp
@@ -26,7 +26,7 @@ Copyright (c) 2025 RRST-NHK-Project. All rights reserved.
 
 // スティックのデッドゾーン
 #define DEADZONE_L 0.3
-#define DEADZONE_R 0.3
+#define DEADZONE_R 0.1
 
 class HardWareControl : public rclcpp::Node
 {
@@ -164,81 +164,189 @@ private:
             CROSS_PUSH_COUNT = (CROSS_PUSH_COUNT + 1) % CROSS_PUSH_MAX;
         }
         if (CROSS_PUSH_COUNT == 0) {
-            data_[16] = 270;
+            data_[9] = 270;
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[1];
+                data_[9] = MAG_CAT_SERVO_ANGLE[0];
             }
         }
         if (CROSS_PUSH_COUNT == 1) {
-            data_[16] = MAG2_SERVO_ANGLE[1];
+            data_[9] = MAG2_SERVO_ANGLE[1];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[1];
+                data_[9] = MAG_CAT_SERVO_ANGLE[1];
             }
         }
         if (CROSS_PUSH_COUNT == 2) {
-            data_[16] = MAG2_SERVO_ANGLE[2];
+            data_[9] = MAG2_SERVO_ANGLE[2];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[2];
+                data_[9] = MAG_CAT_SERVO_ANGLE[2];
             }
         }
         if (CROSS_PUSH_COUNT == 3) {
-            data_[16] = MAG2_SERVO_ANGLE[3];
+            data_[9] = MAG2_SERVO_ANGLE[3];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[3];
+                data_[9] = MAG_CAT_SERVO_ANGLE[3];
             }
         }
+        if (CROSS_PUSH_COUNT == 4) {
+            data_[9] = MAG2_SERVO_ANGLE[3];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[3];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 5) {
+            data_[9] = MAG2_SERVO_ANGLE[3];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[3];
+            }
+        }   
         if (CROSS_PUSH_COUNT == 6) {
-            data_[16] = MAG2_SERVO_ANGLE[4];
+            data_[9] = MAG2_SERVO_ANGLE[4];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[4];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 7) {
+            data_[9] = MAG2_SERVO_ANGLE[4];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[4];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 8) {
+            data_[9] = MAG2_SERVO_ANGLE[4];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
                 data_[16] = MAG_CAT_SERVO_ANGLE[4];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 9) {
+            data_[16] = MAG2_SERVO_ANGLE[4];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[4];
             }
         }
         if (CROSS_PUSH_COUNT == 10) {
-            data_[16] = MAG2_SERVO_ANGLE[6];
+            data_[9] = MAG2_SERVO_ANGLE[6];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[6];
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 11) {
+            data_[9] = MAG2_SERVO_ANGLE[6];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 12) {
+            data_[9] = MAG2_SERVO_ANGLE[6];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 13) {
+            data_[9] = MAG2_SERVO_ANGLE[6];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
             }
         }
         if (CROSS_PUSH_COUNT == 14) {
+            data_[9] = MAG2_SERVO_ANGLE[5];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[5];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 15) {
             data_[16] = MAG2_SERVO_ANGLE[5];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
                 data_[16] = MAG_CAT_SERVO_ANGLE[5];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 16) {
+            data_[9] = MAG2_SERVO_ANGLE[5];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[5];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 17) {
+            data_[9] = MAG2_SERVO_ANGLE[5];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[5];
             }
         }
         if (CROSS_PUSH_COUNT == 18) {
-            data_[16] = MAG2_SERVO_ANGLE[6];
+            data_[9] = MAG2_SERVO_ANGLE[6];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[6];
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 19) {
+            data_[9] = MAG2_SERVO_ANGLE[6];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
             }
         }
         if (CROSS_PUSH_COUNT == 20) {
-            data_[16] = MAG2_SERVO_ANGLE[5];
+            data_[9] = MAG2_SERVO_ANGLE[5];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[5];
+                data_[9] = MAG_CAT_SERVO_ANGLE[5];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 21) {
+            data_[9] = MAG2_SERVO_ANGLE[5];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[5];
             }
         }
         if (CROSS_PUSH_COUNT == 22) {
-            data_[16] = MAG2_SERVO_ANGLE[6];
+            data_[9] = MAG2_SERVO_ANGLE[6];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[6];
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 23) {
+            data_[9] = MAG2_SERVO_ANGLE[6];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[6];
             }
         }
         if (CROSS_PUSH_COUNT == 24) {
-            data_[16] = MAG2_SERVO_ANGLE[3];
+            data_[9] = MAG2_SERVO_ANGLE[3];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[3];
+                data_[9] = MAG_CAT_SERVO_ANGLE[3];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 25) {
+            data_[9] = MAG2_SERVO_ANGLE[3];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[3];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 26) {
+            data_[9] = MAG2_SERVO_ANGLE[3];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[3];
             }
         }
         if (CROSS_PUSH_COUNT == 27) {
-            data_[16] = MAG2_SERVO_ANGLE[4];
+            data_[9] = MAG2_SERVO_ANGLE[4];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[4];
+                data_[9] = MAG_CAT_SERVO_ANGLE[4];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 28) {
+            data_[9] = MAG2_SERVO_ANGLE[4];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[4];
+            }
+        }
+        if (CROSS_PUSH_COUNT == 29) {
+            data_[9] = MAG2_SERVO_ANGLE[4];
+            if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
+                data_[9] = MAG_CAT_SERVO_ANGLE[4];
             }
         }
         if (CROSS_PUSH_COUNT == 30) {
-            data_[16] = MAG2_SERVO_ANGLE[5];
+            data_[9] = MAG2_SERVO_ANGLE[5];
             if (RS_X < 0 && fabs(RS_X) >= DEADZONE_R || R2 == 1) {
-                data_[16] = MAG_CAT_SERVO_ANGLE[5];
+                data_[9] = MAG_CAT_SERVO_ANGLE[5];
             }
         }
 
@@ -255,14 +363,14 @@ private:
             triangle_count = (triangle_count + 1) % triangle_max;
         }
         if (triangle_count == 0) {
-            data_[14] = 0;
-            data_[13] = 10;
+            data_[10] = 25;
+            data_[11] = 120;
         }
         if (triangle_count == 1) {
-            data_[13] = 0;
+            data_[10] = 0;
         }
         if (triangle_count == 2) {
-            data_[14] = 90;
+            data_[11] = 30;
         }
 
         triangle_pre = TRIANGLE;
